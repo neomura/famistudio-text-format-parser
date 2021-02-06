274951102d49feaecaa9742f7274ab74a199d725
jameswilddev <jameswilddev@outlook.com>
2021-02-06 14:12:41 +0000

    Add note value export.

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export { Line, AttributeValue } from "./line";
 export { TextToLineTransform, FormatError } from "./text-to-line-transform";
+export { NoteValue, noteValues } from "./note-value";
---
+++ note-value/unit.ts
@@ -0,0 +1,3 @@
+import { noteValues } from "..";
+
+noteValues;
